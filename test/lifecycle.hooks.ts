f466ff0628dbf609a2ed27d7fca6eb74754a132d
jonas.buerger <jonas.buerger@gutscheinbuch.de>
2024-07-09 16:25:17 +0200

    chore: Add tests to workflow

--- test/lifecycle.hooks.ts
+++ test/lifecycle.hooks.ts
@@ -2,10 +2,6 @@ import { beforeAll, afterAll } from "bun:test";
 
 beforeAll(async () => {
   await Bun.spawn({
-    cmd: ["bun", "install"],
-    stdout: null,
-  }).exited;
-  await Bun.spawn({
     cmd: ["bun", "run", "build"],
     stdout: null,
   }).exited;
