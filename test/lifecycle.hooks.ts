8db7223ed2476cfe97b44e394a8f4ccc8f327c76
jonas.buerger <jonas.buerger@gutscheinbuch.de>
2024-07-11 09:34:44 +0200

    chore: formatting

--- test/lifecycle.hooks.ts
+++ test/lifecycle.hooks.ts
@@ -5,7 +5,7 @@ beforeAll(async () => {
     cmd: ["bun", "run", "build"],
     stdout: "inherit",
   }).exited;
-  if(!await Bun.file('test/project/bun.lockb').exists()) {
+  if (!(await Bun.file("test/project/bun.lockb").exists())) {
     await Bun.spawn({
       cmd: ["bun", "install"],
       cwd: process.cwd() + "/test/project",
